629bb60df3dbea84c51e19b7709af2a087fbcb55
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-10-01 18:18:41 +0530

    Layout & Header setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      <h1 className="m-auto text-4xl font-bold text-gray-400">
+        Welcome to Stock Market App
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Home = () => (
-  <>
-    <Button>Hello World</Button>
-  </>
-);
-
-export default Home;
