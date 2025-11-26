c99ff50a5f7487e42d27c5739648877e44dfc9c8
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-11-26 18:14:01 +0530

    Implemented Stock Details Page

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -37,7 +37,7 @@ const UserDropdown = ({
           className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
         >
           <Avatar className="h-8 w-8">
-            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+            <AvatarImage src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name[0]}
             </AvatarFallback>
@@ -53,7 +53,7 @@ const UserDropdown = ({
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+              <AvatarImage src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg" />
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                 {user.name[0]}
               </AvatarFallback>
